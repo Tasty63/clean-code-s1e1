d34b936ce7d00c61f989858ef027774ec7e2c0ce
Tasty <sapegin.petya@yandex.ru>
2021-03-25 17:36:00 +0400

    refactor: rule 3.4) replace tag selectors

--- app.js
+++ app.js
@@ -33,18 +33,20 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='todo__task-name';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task task_hidden";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit-btn";
 
     deleteButton.className="delete-btn";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className ='delete-btn__image';
+
     deleteButton.appendChild(deleteButtonImg);
 
 
